61dd2c0972ede21fcaf316c6f01309ea68b6f141
Yaroslava <yaroslava.ivanivna9@gmail>
2024-12-15 13:14:00 +0200

    task 5 is completed

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.body;
+const colorSpan = document.querySelector(".color");
+btnChangeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
